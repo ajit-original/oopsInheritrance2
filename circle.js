ea6388e247610533caddc9006324bc03146131bc
Ajit Singh <86841688+ajitoriginal@users.noreply.github.com>
2022-01-04 17:21:27 +0530

    Add files via upload

---
+++ circle.js
@@ -0,0 +1,11 @@
+import Shape from './shape'
+
+module.exports = class Circle extends Shape{
+    constructor(parameters){
+        super(color);
+    }
+    calculateArea() {
+        return this.color
+    }
+
+}
---
+++ shape.js
@@ -0,0 +1,15 @@
+module.exports = class Shape {
+    constructor(parameters){
+        this.color = "black";
+    }
+    drawShape() {
+
+    }
+    calculateArea() {
+        return this.color
+    }
+    colorProperty() {
+        color = black;
+    }
+}
+
